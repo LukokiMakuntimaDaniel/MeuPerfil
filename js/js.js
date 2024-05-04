6259743c4d901a922b6f63c67efb955e60a6ef73
LukokiMakuntimaDaniel <lukokimakuntimadaniel@gmail.com>
2024-05-04 22:32:10 +0100

    Adiciona função de atualização de dados do usuário

--- js/js.js
+++ js/js.js
@@ -1,7 +1,7 @@
 iniciar();
 
 function iniciar() {
-    usuario = JSON.parse(localStorage.getItem("user"))
+    usuario = JSON.parse(localStorage.getItem("usuario"))
     if (usuario) {
         document.getElementById("nome").innerHTML = usuario.nome
         document.getElementById("idade").innerHTML = usuario.idade + " anos"
@@ -25,3 +25,33 @@ function iniciar() {
 
 }
 
+function actualizarDados() {
+    usuario = JSON.parse(localStorage.getItem("usuario"))
+    usuario.nome = (document.getElementById("nomeInput").value) ? document.getElementById("nomeInput").value : (usuario) ? usuario.nome : ""
+    usuario.idade = (document.getElementById("idadeInput").value) ? document.getElementById("idadeInput").value : (usuario) ? usuario.nome : ""
+    usuario.rua = (document.getElementById("ruaInput").value) ? document.getElementById("ruaInput").value : (usuario) ? usuario.nome : ""
+    usuario.bairro = (document.getElementById("bairroInput").value) ? document.getElementById("bairroInput").value : (usuario) ? usuario.nome : ""
+    usuario.estado = (document.getElementById("estadoInput").value) ? document.getElementById("estadoInput").value : (usuario) ? usuario.nome : ""
+    usuario.biografia = (document.getElementById("rua").value) ? document.getElementById("rua").value : (usuario) ? usuario.nome : ""
+    usuario.telefone = (document.getElementById("telemovelInput").value) ? document.getElementById("telemovelInput").value : (usuario) ? usuario.nome : ""
+    usuario.biografia = (document.getElementById("biografiaInput").value) ? document.getElementById("biografiaInput").value : (usuario) ? usuario.nome : ""
+
+    let imagemValue = document.getElementById("img_p").value;
+    if (imagemValue) {
+        let imagemInput = document.getElementById("img_p")
+        let file = new FileReader();
+        file.readAsDataURL(imagemInput.files[0]);
+        file.onloadend = function (captura) {
+            usuario.imagem = captura.target.result;
+            usuarioString = JSON.stringify(usuario);
+            localStorage.setItem('usuario', usuarioString);
+            iniciar()
+        }
+    } else {
+        usuarioString = JSON.stringify(usuario);
+        localStorage.setItem('usuario', usuarioString);
+        iniciar()
+    }
+
+
+}
