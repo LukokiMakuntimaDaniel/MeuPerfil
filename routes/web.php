f4f04000dd6f0fe0ad472a81a369ba291896ddee
LukokiMakuntimaDaniel <lukokimakuntimadaniel@gmail.com>
2024-05-05 17:11:54 +0100

    criando as rotas

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UsuarioController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('index');
 });
+
+
+
+Route::prefix("usuario")->group(function(){
+    Route::post('/salvar', [UsuarioController::class, 'store'])->name("usuario.store");
+    Route::get('/', [UsuarioController::class, 'index']);
+    Route::put('/actualizar', [UsuarioController::class, 'update'])->name("usuario.salvar");
+    Route::delete('deletar/{id}', [UsuarioController::class, 'destroy'])->name("usuario.deletar");;
+});
+
+
